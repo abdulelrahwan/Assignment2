53acae6424df8d9f674c1d97952131fd7632968e
Abdul <abdulelrahwan@gmail.com>
2020-10-04 22:23:16 -0400

    gkgfkgf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" style="background-color:#0064a2">
+     <section>
+				<div style="padding-top:20px" class="block">
+        
+        
+              <b-checkbox v-model="displayName">
+                  Name
+              </b-checkbox>
+  
+              <b-checkbox v-model="displayRelationship">
+                  Relationshop
+              </b-checkbox>
+					</div>
+    </section>
+    <b-carousel :autoplay=false style="height:600px">
+        <b-carousel-item v-for="(person, i) in people" :key="i">
+            <section class="hero">
+                <div class="hero-body has-text-centered">
+									<!-- <div class="myDIV"> -->
+                    <img class="myDIV" :src="getImgUrl(person.photo)"  style="width:300px;height:300px;"/>
+									<!-- </div> -->
+										<div class="hide">
+											<p v-if="displayName">{{person.name}}</p>
+											<p v-if="displayRelationship">{{person.relationship}}</p>
+										</div>
+                    
+                </div>
+            </section>
+        </b-carousel-item>
+    </b-carousel>
   </div>
 </template>
 
@@ -35,6 +37,25 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      people: [{name: 'Beth', photo: 'grandchild1.jpg', relationship: 'Granddaughter', number: '337-444-5555'},
+      {name: 'Martin', photo: 'brother.jpg', relationship: 'Brother',  number: '337-444-5555'},
+      {name: 'Harry', photo: 'grandchild2.jpg', relationship: 'Grandson',  number: '337-444-5555'},
+      {name: 'Joy', photo: 'neice1.jpg', relationship: 'Neice',  number: '337-444-5555'},
+      {name: 'Sophia', photo: 'neice2.jpg', relationship: 'Neice',  number: '337-444-5555'},
+      {name: 'Olivia', photo: 'neice3.jpg', relationship: 'Neice',  number: '337-444-5555'},
+      {name: 'John', photo: 'nephew1.jpg', relationship: 'Nephew',  number: '337-444-5555'},
+      {name: 'Edward', photo: 'nephew2.jpg', relationship: 'Nephew',  number: '337-444-5555'}],
+      displayRelationship: true,
+      displayName: true
+    }
+  },
+  methods: {
+    getImgUrl(photo) { 
+      return require("../../" + photo);
+    }
   }
 }
 </script>
@@ -54,5 +75,13 @@ li {
 }
 a {
   color: #42b983;
+}
+.hide {
+  display: none;
+}
+
+.myDIV:hover + .hide {
+  display: block;
+  color: red;
 }
 </style>
